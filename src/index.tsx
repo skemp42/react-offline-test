c011a69cce21d24fb62b78fa8cdac5143edd09c0
Stephen Kemp <steve94kemp@gmail.com>
2019-09-17 18:38:59 +0100

    Move app.tsx to Components folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./compoennts/app";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(<App />, document.getElementById("reactMountPoint"));
