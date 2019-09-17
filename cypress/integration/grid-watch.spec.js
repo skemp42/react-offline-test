824c2c57790469bc6cb03d2609f9d94313c23c50
Stephen Kemp <steve94kemp@gmail.com>
2019-09-17 20:36:45 +0100

    GridWatch calls getGenerations and displays from date

--- cypress/integration/grid-watch.spec.js
+++ cypress/integration/grid-watch.spec.js
@@ -1,12 +1,22 @@
 describe("grid watch", () => {
   beforeEach(() => {
+    cy.server();
+    cy.route(
+      "GET",
+      "https://api.carbonintensity.org.uk/generation",
+      "fixture:generation-response.json"
+    );
     cy.visit("/");
   });
 
-  it("shows grid watch heading", () => {
-    cy.findByTestId("grid-watch").should(
+  it("shows grid watch heading with from date", () => {
+    cy.findByTestId("grid-watch").as("grid");
+
+    cy.get("@grid").should(
       "contain",
       "GB Electricity National Grid Demand and Output per Production Type"
     );
+
+    cy.get("@grid").should("contain", "Aug 12th 2019");
   });
 });
--- src/features/grid-watch/GridWatch.tsx
+++ src/features/grid-watch/GridWatch.tsx
@@ -1,11 +1,36 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState, useEffect } from "react";
+import { getGenerations } from "./gridWatchActions";
+import GenerationMixResponse from "./models/GenerationMixResponse";
+import { Col, Row } from "antd";
+import moment from "moment";
 
 const GridWatch = (): ReactElement => {
+  const [generationMix, setGenerationMix] = useState<
+    GenerationMixResponse | undefined
+  >(undefined);
+
+  useEffect(() => {
+    getGenerations().then((res) => setGenerationMix(res));
+  }, []);
+
   return (
     <section data-testid='grid-watch'>
-      <h1>
-        GB Electricity National Grid Demand and Output per Production Type
-      </h1>
+      <Row justify='center' type='flex' gutter={4}>
+        <Col>
+          <h1>
+            GB Electricity National Grid Demand and Output per Production Type
+          </h1>
+        </Col>
+      </Row>
+      <Row justify='center' type='flex'>
+        {generationMix && (
+          <>
+            <Col>
+              {moment(generationMix.data.from).format("MMM Do YYYY")} <hr />
+            </Col>
+          </>
+        )}
+      </Row>
     </section>
   );
 };
