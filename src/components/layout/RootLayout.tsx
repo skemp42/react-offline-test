55a06d541581e67fc6b988ecaf8ce680de479847
Stephen Kemp <steve94kemp@gmail.com>
2019-09-17 21:38:43 +0100

    Remove unused navbar

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,16 +1,12 @@
 import React, { ReactElement } from "react";
 import { Layout } from "antd";
 import GridWatch from "../../features/grid-watch/GridWatch";
-import NavMenu from "./NavMenu";
 
-const { Sider, Content } = Layout;
+const { Content } = Layout;
 
 const RootLayout = (): ReactElement => {
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Sider style={{ marginRight: 20 }}>
-        <NavMenu />
-      </Sider>
       <Content>
         {
           //Normally routes would go in here!
